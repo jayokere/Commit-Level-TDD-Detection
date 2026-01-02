d8581186fb30d430d8468c34f74b887a7ac08d7d
Jermaine Acho Okere <jayokere012@gmail.com>
2026-01-02 20:34:50 +0000

    refactor: streamline imports and function calls in main.py

--- main.py
+++ main.py
@@ -2,20 +2,21 @@
 from pydriller import Repository
 
 # Internal Modules
-import miner_intro
-import apache_miner
-import clean_db
+from miner_intro import run_all as miner_intro
+from apache_miner import run_all as apache_miner
+from clean_db import run as clean_db
 from db import get_collection
 from utils import measure_time
 from repo_miner import Repo_miner 
+from sync_counts import run_sync_counts
 
 # Main function
 @measure_time
 def main() -> None:
-    miner_intro.run_all()
+    miner_intro()
 
     # Run Apache GitHub Miner
-    apache_miner.run_all()
+    apache_miner()
 
     # Get summary from Database
     project_count = get_collection("mined-repos").count_documents({})
@@ -25,7 +26,9 @@ def main() -> None:
     Repo_miner().run()
 
     # Clean duplicate commits from Database
-    clean_db.run()
+    clean_db()
+
+    run_sync_counts()
 
 if __name__ == "__main__":
     main()
