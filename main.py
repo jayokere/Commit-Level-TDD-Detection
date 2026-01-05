40393eca31b6858ae878666700d6f59a375f9399
Jermaine Acho Okere <jayokere012@gmail.com>
2026-01-05 14:55:30 +0000

    feat: implement run_analysis module to encapsulate analysis logic

--- main.py
+++ main.py
@@ -4,15 +4,10 @@ from pydriller import Repository
 # Internal Modules
 from utilities.miner_intro import run_all as miner_intro
 from mining.apache_miner import run_all as apache_miner
-from database.clean_db import run as clean_db
 from database.db import get_collection
 from utilities.utils import measure_time
 from mining.repo_miner import Repo_miner 
-from database.sync_counts import run_sync_counts
-from analysis.source_file_calculator import run_calculator
-from analysis.lifecycle_analysis import run as lifecycle_analysis
-from analysis.creation_analysis import run as creation_analysis
-from analysis.static_analysis import run as static_analysis
+from analysis.run_analysis import main as run_analysis
 
 # Main function
 @measure_time
@@ -29,18 +24,8 @@ def main() -> None:
     # Run Repository Miner
     Repo_miner().run()
 
-    # Clean duplicate commits from Database
-    clean_db()
-
-    run_calculator()
-
-    run_sync_counts()
-
-    static_analysis("4")
-
-    lifecycle_analysis("4")
-
-    creation_analysis("4")
+    # Run Analysis Modules
+    run_analysis()
 
 if __name__ == "__main__":
     main()
